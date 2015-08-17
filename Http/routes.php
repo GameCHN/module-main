8b422619207bc8d3a0677b9c8f4a9e29eb3572df
Mo <mo@gbjobs.com>
2015-08-17 11:52:24 +0800

    new route

--- Http/routes.php
+++ Http/routes.php
@@ -2,6 +2,16 @@
 
 
 Route::group(['prefix' => '', 'namespace' => 'YC\Main\Http\Controllers'], function () {
+
+    global $posts, $post, $wp_did_header, $wp_query, $wp_rewrite, $wpdb, $wp_version, $wp, $id, $comment, $user_ID;
+
+
+    if (PHP_SAPI != 'cli' && WP_ONLY == false) {
+        /** Loads the WordPress Environment and Template */
+        require(ROOT . '/home/wp-blog-header.php');
+        require_once WP_CORE_DIR.'/init.php';
+    }
+
     Route::get('/', 'MainController@index');
     Route::any('{all?}', 'MainController@index')->where('all', '.+');
 });
